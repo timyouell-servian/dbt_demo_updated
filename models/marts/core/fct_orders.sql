0774b0ac5776f4993bea0092d2260fd665a42758
Emerson Ferreira <emerson.ferreira.net@gmail.com>
2024-02-13 04:04:03 +0000

    Implementing the compare Month over Month comparison

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,30 +1,28 @@
-with orders as (
-    select * from {{ ref('stg_orders' ) }}
-),
+with
+    orders as (select * from {{ ref("stg_orders") }}),
 
-payments as (
-    select * from {{ ref('stg_payments') }}
-),
+    payments as (select * from {{ ref("stg_payments") }}),
 
-order_payments as (
-    select
-        order_id,
-        sum(case when status = 'success' then amount end) as amount
+    order_payments as (
+        select order_id, sum(case when status = 'completed' then amount end) as amount
 
-    from payments
-    group by 1
-),
+        from payments
+        group by 1
+    ),
 
-final as (
+    final as (
 
-    select
-        orders.order_id,
-        orders.customer_id,
-        orders.order_date,
-        coalesce(order_payments.amount, 0) as amount
+        select
+            orders.order_id,
+            orders.customer_id,
+            orders.order_date,
+            coalesce(order_payments.amount, 0) + 10 as amount,
+            getdate() as updated_at
 
-    from orders
-    left join order_payments on orders.order_id = order_payments.order_id
-)
+        from orders
+        left join order_payments on orders.order_id = order_payments.order_id
+    )
 
-select * from final
+select *
+from final
+where date_part(month, order_date) <= 4
