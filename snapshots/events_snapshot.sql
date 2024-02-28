e53acb4c6e67ba53e750e762b6a05308d4136a99
Tim Youell <timothy.youell@cognizant.com>
2024-02-28 06:19:32 +0000

    aggregate testing added

---
+++ snapshots/events_snapshot.sql
@@ -0,0 +1,14 @@
+{% snapshot fct_orders_snapshot %}
+
+{{
+    config(
+      target_schema=env_var('DBT_SNP_SCHEMA'),
+      unique_key='event_id',
+      strategy='timestamp',
+      updated_at='event_date',
+    )
+}}
+
+select * from {{ ref('events_table') }}
+
+{% endsnapshot %}
